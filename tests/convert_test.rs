d99b87ba475e2b453ff9eb42bea08f76a0d65148
Jonas Bushart <jonas@bushart.org>
2023-07-01 16:43:59 +0200

    update test fixtures

--- tests/convert_test.rs
+++ tests/convert_test.rs
@@ -14,10 +14,10 @@ fn test_convert_fixture() {
     let assert = assert.success();
     let output = assert.get_output();
     let expected = expect_test::expect![[r#"
-        <?xml version="1.0" encoding="utf-8"?><testsuites><testsuite id="0" name="pre-commit" package="testsuite/pre-commit" tests="6" errors="2" failures="0" hostname="localhost" timestamp="1970-01-01T00:00:00Z" time="1.399999999"><testcase name="Hook which always passes" time="0" classname="passing"><system-out><![CDATA[
+        <?xml version="1.0" encoding="utf-8"?><testsuites><testsuite id="0" name="pre-commit" package="testsuite/pre-commit" tests="6" errors="2" failures="0" hostname="localhost" timestamp="1970-01-01T00:00:00Z" time="1.4"><testcase name="Hook which always passes" time="0" classname="passing"><system-out><![CDATA[
         success
 
-        ]]></system-out></testcase><testcase name="Hook which always fails" time="0" classname="failing"><error type="Exit Code" message="1"><![CDATA[]]></error></testcase><testcase name="Slow hook for duration" time="1.399999999" classname="slow"><system-out><![CDATA[]]></system-out></testcase><testcase name="Hook does not run because no files" time="0" classname="missing-files"><skipped/></testcase><testcase name="Hook skipped by environment" time="0" classname="skipped"><skipped/></testcase><testcase name="Modifies files" time="0" classname="modifies"><error type="Modified Files" message=""><![CDATA[]]></error></testcase></testsuite></testsuites>"#]];
+        ]]></system-out></testcase><testcase name="Hook which always fails" time="0" classname="failing"><error type="Exit Code" message="1"><![CDATA[]]></error></testcase><testcase name="Slow hook for duration" time="1.4" classname="slow"><system-out><![CDATA[]]></system-out></testcase><testcase name="Hook does not run because no files" time="0" classname="missing-files"><skipped/></testcase><testcase name="Hook skipped by environment" time="0" classname="skipped"><skipped/></testcase><testcase name="Modifies files" time="0" classname="modifies"><error type="Modified Files" message=""><![CDATA[]]></error></testcase></testsuite></testsuites>"#]];
     expected.assert_eq(std::str::from_utf8(&output.stdout).unwrap());
 }
 
@@ -35,10 +35,10 @@ fn test_convert_fixture_with_diff() {
     let assert = assert.success();
     let output = assert.get_output();
     let expected = expect_test::expect![[r#"
-        <?xml version="1.0" encoding="utf-8"?><testsuites><testsuite id="0" name="pre-commit" package="testsuite/pre-commit" tests="6" errors="2" failures="0" hostname="localhost" timestamp="1970-01-01T00:00:00Z" time="1.399999999"><testcase name="Hook which always passes" time="0" classname="passing"><system-out><![CDATA[
+        <?xml version="1.0" encoding="utf-8"?><testsuites><testsuite id="0" name="pre-commit" package="testsuite/pre-commit" tests="6" errors="2" failures="0" hostname="localhost" timestamp="1970-01-01T00:00:00Z" time="1.4"><testcase name="Hook which always passes" time="0" classname="passing"><system-out><![CDATA[
         success
 
-        ]]></system-out></testcase><testcase name="Hook which always fails" time="0" classname="failing"><error type="Exit Code" message="1"><![CDATA[]]></error></testcase><testcase name="Slow hook for duration" time="1.399999999" classname="slow"><system-out><![CDATA[]]></system-out></testcase><testcase name="Hook does not run because no files" time="0" classname="missing-files"><skipped/></testcase><testcase name="Hook skipped by environment" time="0" classname="skipped"><skipped/></testcase><testcase name="Modifies files" time="0" classname="modifies"><error type="Modified Files" message=""><![CDATA[]]></error></testcase><system-out><![CDATA[pre-commit hook(s) made changes.
+        ]]></system-out></testcase><testcase name="Hook which always fails" time="0" classname="failing"><error type="Exit Code" message="1"><![CDATA[]]></error></testcase><testcase name="Slow hook for duration" time="1.4" classname="slow"><system-out><![CDATA[]]></system-out></testcase><testcase name="Hook does not run because no files" time="0" classname="missing-files"><skipped/></testcase><testcase name="Hook skipped by environment" time="0" classname="skipped"><skipped/></testcase><testcase name="Modifies files" time="0" classname="modifies"><error type="Modified Files" message=""><![CDATA[]]></error></testcase><system-out><![CDATA[pre-commit hook(s) made changes.
         If you are seeing this message in CI, reproduce locally with: `pre-commit run --all-files`.
         To run `pre-commit` as part of git workflow, use `pre-commit install`.
         All changes made by hooks:
